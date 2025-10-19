b6ee4e1571e782f663d7deeefca2fba9b84bec6c
Irene Chebet <irenetowett201@gmail.com>
2025-10-19 23:10:07 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
 -- Select the database
 USE alx_book_store;
 
--- Display the full description of the table 'books'
-SHOW CREATE TABLE books;
+-- Display the full description of the table 'Books'
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
